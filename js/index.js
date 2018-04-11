a5bb3064286f6f302980c90b4f372f8627112c4d
Philip Langbehn <langbehn.phil@gmail.com>
2018-04-11 15:29:44 +0200

    added new boilerplate based on github.com/luketep/clean

--- js/index.js
+++
@@ -1,9 +0,0 @@
-// create namespace
-window._sw = {};
-
-window.onload = function() {
-    window.__T = setTimeout(function() {
-        document.getElementById('status').innerText = 'loaded';    
-        clearTimeout(window.__T);
-    }, 1000);    
-}
